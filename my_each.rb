f97d6f741d15250d3d0a3289e15082ea02d236ca
Kelliann Lafferty <kell10192@gmail.com>
2019-06-10 17:09:11 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+  n = 0
+  while n < array.length 
+  yield array[n]
+  n = n + 1 
+end 
+array 
+end 
+
+# does not call on .each 
+# does not call on puts 
+# calls on while
+# iterates over each element
+# yields the correct element
+# can handle an empty collection 
+# returned array contains the same elements as the original collection (.each) 
+# does not modify the original collection 
+# block is run n times 
+# only passes a single element into the block at a time 
