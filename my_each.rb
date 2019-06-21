35eddee75197d36440eadc33cc2ffb7680d45d28
evancarlsen <evan@evancarlsen.com>
2019-06-21 11:56:06 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
+  i = 0
   # code here
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
 end
+
